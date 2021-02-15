356c8bde24c14210150c8abb12a4ac7f3fded176
irwinfrimpong <frimponi@lafayette.edu>
2021-02-14 23:44:11 -0500

    Lab01 part 1 comeplete

---
+++ Lab01_7Seg_LED_Ctrller/Lab01_7Seg_LED_Ctrller.srcs/sources_1/new/sevenseg_ext.sv
@@ -0,0 +1,72 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date: 02/11/2021 01:53:18 PM
+// Design Name:
+// Module Name: sevenseg_ext
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sevenseg_ext(
+    input logic [6:0]data,
+    output logic [6:0] segs_n,  // ordered g(6) - a(0)
+    output logic dp_n // decimal point
+    );
+
+    // data[6]: blank
+    //data[5]: dp
+    //data[4]: dash
+    //data[3:0]: binaryvalue
+
+    always_comb
+    begin
+        // Decimal Point for when dp is asserted
+        dp_n = data[5];
+        //blanks when data[6] is asserted
+        if(data[6])
+            begin
+                segs_n = 7'b1111111;
+                dp_n = 1'b1;
+            end 
+        //display dash/minus sign
+        else if(data[4])
+        segs_n = 7'b0111111;
+        else
+        begin
+            //converts 4-digit binary to 1-digit hex
+            case (data[3:0])     //  gfedcba
+                4'd0: segs_n = 7'b1000000;
+                4'd1: segs_n = 7'b1111001;
+                4'd2: segs_n = 7'b0100100;
+                4'd3: segs_n = 7'b0110000;
+                4'd4: segs_n = 7'b0011001;
+                4'd5: segs_n = 7'b0010010;
+                4'd6: segs_n = 7'b0000010;
+                4'd7: segs_n = 7'b1111000;
+                4'd8: segs_n = 7'b0000000;
+                4'd9: segs_n = 7'b0010000;
+                4'd10: segs_n = 7'b0001000;
+                4'd11: segs_n = 7'b0000011;
+                4'd12: segs_n = 7'b1000110;
+                4'd13: segs_n = 7'b0100001;
+                4'd14: segs_n = 7'b0000110;
+                4'd15: segs_n = 7'b0001110;
+                default: segs_n = 7'b1111111;
+            endcase
+        end
+    end
+
+endmodule
