a261ec8c7217d9f2b3b21c20c6143e2d682df37e
irwinfrimpong <frimponi@lafayette.edu>
2021-03-11 00:05:29 -0500

    transmitter and sctb complete

--- lab03_manchestor_transmitter/lab03_manchestor_transmitter.srcs/sources_1/new/manchester_sctb.sv
+++ lab03_manchestor_transmitter/lab03_manchestor_transmitter.srcs/sources_1/new/manchester_sctb.sv
@@ -10,7 +10,7 @@
 // Target Devices:
 // Tool Versions:
 // Description:
-// 
+//
 // Dependencies:
 //
 // Revision:
@@ -77,26 +77,130 @@ module manchester_sctb (
         end while (rdy == 1);
         valid = 0;
         wait(txen == 1);
-//        #(BITPD_NS/4);
+        //        #(BITPD_NS/4);
         for (int i = 0; i <= 15; i++) begin
-//            check(txd, ~txd, rdy, 0, txen, 1);
-//            if(i % 2) $display("%t inverted d", $time);
-//            else $display("%t regular d", $time);
+            //            check(txd, ~txd, rdy, 0, txen, 1);
+            //            if(i % 2) $display("%t inverted d", $time);
+            //            else $display("%t regular d", $time);
+            $display("Checking i:%d at t:%t",i,$time);
             if(i % 2) check(txd, ~d[i/2], rdy, 0, txen, 1);
             else check(txd, d[i/2], rdy, 0, txen, 1);
             #(BITPD_NS/2);
         end
+
     endtask
-    // place additional transaction tasks here
+
+    task multi_transmission( logic [7:0] d1,d2,d3,d4,d5);
+        valid = 1'b1;
+        data = d1;
+        do begin
+            @(posedge clk);
+        end while (rdy == 0);
+        #1 valid = 0;
+        do begin
+            @(posedge clk) #1;
+        end while (rdy == 1);
+        valid = 0;
+        wait(txen == 1);
+        //        #(BITPD_NS/4);
+        $display("1st Transmission");
+        for (int i = 0; i <= 16; i++) begin
+            //            check(txd, ~txd, rdy, 0, txen, 1);
+            //            if(i % 2) $display("%t inverted d", $time);
+            //            else $display("%t regular d", $time);
+            if(i % 2) check(txd, ~d1[i/2], rdy, 0, txen, 1);
+            else check(txd, d1[i/2], rdy, 0, txen, 1);
+            if(i !=15) #(BITPD_NS/2);
+            else
+            begin
+                data= d2;
+                #1 valid = 1'b1;
+            end
+        end
+        do begin
+            @(posedge clk) #1;
+        end while (rdy == 1);
+        #1 valid = 0;
+        $display("2nd Transmission");
+        for (int i = 0; i <= 16; i++) begin
+            //            check(txd, ~txd, rdy, 0, txen, 1);
+            //            if(i % 2) $display("%t inverted d", $time);
+            //            else $display("%t regular d", $time);
+            if(i % 2) check(txd, ~d2[i/2], rdy, 0, txen, 1);
+            else check(txd, d2[i/2], rdy, 0, txen, 1);
+            if(i !=15) #(BITPD_NS/2);
+            else
+            begin
+                data= d3;
+                #1 valid = 1'b1;
+            end
+        end
+        do begin
+            @(posedge clk) #1;
+        end while (rdy == 1);
+        #1 valid = 0;
+        $display("3rd Transmission");
+        for (int i = 0; i <= 16; i++) begin
+            //            check(txd, ~txd, rdy, 0, txen, 1);
+            //            if(i % 2) $display("%t inverted d", $time);
+            //            else $display("%t regular d", $time);
+            if(i % 2) check(txd, ~d3[i/2], rdy, 0, txen, 1);
+            else check(txd, d3[i/2], rdy, 0, txen, 1);
+            if(i !=15) #(BITPD_NS/2);
+            else
+            begin
+                data= d4;
+                #1 valid = 1'b1;
+            end
+        end
+        do begin
+            @(posedge clk) #1;
+        end while (rdy == 1);
+        #1 valid = 0;
+        $display("4th Transmission");
+        for (int i = 0; i <= 16; i++) begin
+            //            check(txd, ~txd, rdy, 0, txen, 1);
+            //            if(i % 2) $display("%t inverted d", $time);
+            //            else $display("%t regular d", $time);
+            if(i % 2) check(txd, ~d4[i/2], rdy, 0, txen, 1);
+            else check(txd, d4[i/2], rdy, 0, txen, 1);
+            if(i !=15) #(BITPD_NS/2);
+            else
+            begin
+                data= d5;
+                #1 valid = 1'b1;
+            end
+        end
+        do begin
+            @(posedge clk) #1;
+        end while (rdy == 1);
+        #1 valid = 0;
+        $display("5th Transmission");
+
+        for (int i = 0; i <= 16; i++) begin
+            //            check(txd, ~txd, rdy, 0, txen, 1);
+            //            if(i % 2) $display("%t inverted d", $time);
+            //            else $display("%t regular d", $time);
+            if(i % 2) check(txd, ~d5[i/2], rdy, 0, txen, 1);
+            else check(txd, d5[i/2], rdy, 0, txen, 1);
+            if(i !=15) #(BITPD_NS/2);
+        end
+
+    endtask
 
     initial begin
         $timeformat(-9, 0, "ns", 6);
         $monitor( /* add signals to monitor in console */ );
         reset_duv;
 
-        // call transaction tasks here
-        single_transmission(8'b01010101);
-        #(BITPD_NS*2)
+        //$display("Single Transmission");
+        //single_transmission(8'b01010101);
+        //#(BITPD_NS*2)
+        //check(txd,1,rdy,1,txen,0);
+
+         $display("Multi Transmission");
+         multi_transmission(8'b01010101,8'b11110000,8'b01010101,8'b11110000,8'b01010101);
+         #(BITPD_NS*2)
         check(txd,1,rdy,1,txen,0);
         report_errors;
         $finish;  // suspend simulation (use $finish to exit)
